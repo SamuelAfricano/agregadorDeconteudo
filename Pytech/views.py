890146e39a2343a9ddf80d5395301cfd076fcd68
Samuel K Africano <Kandumboafricano@gmail.com>
2022-09-09 17:41:23 +1200

    Actulizando a views:paginação adicionada

--- Pytech/views.py
+++ Pytech/views.py
@@ -5,10 +5,11 @@ from .models import Item
 class HomePageView(ListView):
     template_name = "homepage.html"
     model = Item
+    paginate_by = 3
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["text"] = Item.objects.filter().order_by("-id")[:10]
+        context["text"] = Item.objects.filter().order_by("-id")
         return context
 
 
